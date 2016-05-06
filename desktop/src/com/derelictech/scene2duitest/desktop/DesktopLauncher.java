c7272dbd5ccbd43a1569ff5af58e2d34c95e01de
Tim Slippy, voxelv <slippytim@gmail.com>
2016-05-05 21:34:18 -0700

    init

---
+++ desktop/src/com/derelictech/scene2duitest/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.derelictech.scene2duitest.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.derelictech.scene2duitest.Scene2DUITest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Scene2DUITest(), config);
+	}
+}
